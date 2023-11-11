4d84c9daa10788cc75ce4689e4993c83ce24ed06
Oscar Serna <oscr.serna@gmail.com>
2023-11-11 17:11:42 -0500

    Fix format currency function

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,8 +1,7 @@
 export function formatCurrency(value: number, currency: string = 'USD') {
-  const USDollar = new Intl.NumberFormat('en-US', {
+  const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency,
   })
-
-  return USDollar.format(value)
+  return isNaN(value) ? formatter.format(0) : formatter.format(value)
 }
