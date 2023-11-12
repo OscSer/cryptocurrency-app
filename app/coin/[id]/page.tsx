46807413df9634894b4b0b3e9e856dc1dd7282a7
Oscar Serna <oscr.serna@gmail.com>
2023-11-12 13:31:51 -0500

    Implement coin page

--- app/coin/[id]/page.tsx
+++ app/coin/[id]/page.tsx
@@ -1,11 +1,38 @@
-import styles from "./page.module.css";
+'use client'
+
+import styles from './page.module.css'
+import { useRouter } from 'next/navigation'
+import { useCoinByIdQuery } from '@/lib/redux/features/coinSlice'
+import { ROUTES } from '@/lib/constants'
+import Loading from '@/components/loading/loading'
+import ErrorMessage from '@/components/error-message/error-message'
+import { Button } from '@mui/material'
+import { IconArrowBackUp } from '@tabler/icons-react'
+import CoinDetail from '@/components/coin-detail/coin-detail'
 
 interface Props {
   params: {
-    id: string;
-  };
+    id: string
+  }
 }
 
 export default function CoinPage({ params }: Props) {
-  return <h1>ID: {params.id}</h1>;
+  const router = useRouter()
+  const { data, isLoading, isSuccess, error } = useCoinByIdQuery({
+    id: params.id,
+  })
+
+  const backToHome = () => router.push(ROUTES.HOME)
+
+  if (isLoading) return <Loading />
+  if (error || !data) return <ErrorMessage />
+  if (isSuccess)
+    return (
+      <div className={styles.coinPage}>
+        <Button startIcon={<IconArrowBackUp />} onClick={backToHome}>
+          Home
+        </Button>
+        <CoinDetail coin={data} />
+      </div>
+    )
 }
