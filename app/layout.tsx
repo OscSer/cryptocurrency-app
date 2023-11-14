06effb18c5ad1d626a4741179d2a07ca52babb0d
Oscar Serna <oscr.serna@gmail.com>
2023-11-14 10:32:31 -0500

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ReduxProvider } from '@/lib/redux/provider'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ReduxProvider>{children}</ReduxProvider>
+        <Analytics />
       </body>
     </html>
   )
